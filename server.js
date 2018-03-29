30f4cbf610c93b4e429a5823f251d75cdfd926b2
Vinh Nguyen <luzusomuch@gmail.com>
2018-03-29 13:33:57 +0700

    update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var forceSSL = function() {
 // Instruct the app
 // to use the forceSSL
 // middleware
-// app.use(forceSSL());
+app.use(forceSSL());
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
