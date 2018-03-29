84a8c1c95ff5c569947e6f5dd59b7eb5e580491f
Vinh Nguyen <luzusomuch@gmail.com>
2018-03-29 11:51:37 +0700

    init create challenge

--- app/scripts/app/challenges/create/controller.js
+++ app/scripts/app/challenges/create/controller.js
@@ -3,6 +3,17 @@
   angular.module('measureApp').controller('CreateChallengeController', CreateChallengeController);
 
   /* @ngInject */
-  function CreateChallengeController() {
+  function CreateChallengeController($scope) {
+    $scope.data = {
+      title: '',
+      description: '',
+      announcement: '',
+      expiresAt: new Date()
+    };
+    $scope.dateOptions = {
+      minDate: new Date(),
+    };
+    $scope.submitted = false;
+    $scope.thumbnails = [];
   }
 })();
