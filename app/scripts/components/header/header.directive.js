0b148171f0cad295c5d9bc6977e873c74d5776e4
Vinh Nguyen <luzusomuch@gmail.com>
2018-04-12 11:51:30 +0700

    remove partner in header

--- app/scripts/components/header/header.directive.js
+++ app/scripts/components/header/header.directive.js
@@ -5,14 +5,19 @@ angular.module('measureApp').directive('header', function (AuthService) {
     restrict: 'E',
     templateUrl: 'scripts/components/header/view.html',
     link: function(scope) {
+      // scope.menuItems = [{
+      //   label: 'Partners',
+      //   sref: 'app.partner.list',
+      //   faClass: 'fa fa-user'
+      // }, {
+      //   label: 'Shows',
+      //   sref: 'app.show.list',
+      //   faClass: 'fa fa-server'
+      // }];
       scope.menuItems = [{
         label: 'Partners',
         sref: 'app.partner.list',
         faClass: 'fa fa-user'
-      }, {
-        label: 'Shows',
-        sref: 'app.show.list',
-        faClass: 'fa fa-server'
       }];
     }
   };
